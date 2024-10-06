9acb399ffe90dc0b038a298e4eade26e9a154de1
tuantt <tranthetuan1998hanam@gmail.com>
2024-10-07 00:32:22 +0700

    test: read config from envs

--- config/config.go
+++ config/config.go
@@ -1,7 +1,7 @@
 package config
 
 type AppConfig struct {
-	HTTPPort string `mapstructure:"HTTP_PORT"`
+	HTTPPort string `mapstructure:"MOVIEZ_HTTP_PORT"`
 	DBConfig `mapstructure:",squash"`
 }
 
--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.23.1
 
 require (
 	github.com/gorilla/mux v1.8.1
-	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.8.1
 	github.com/spf13/viper v1.19.0
 )
